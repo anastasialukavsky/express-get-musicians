cb7e590276943dc0f04ce90873741acab13ad1ab
anastasia lukavsky <stacylukavsky@gmail.com>
2024-05-08 13:09:32 -0400

    add body parsers and connected routers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+// const { Musician } = require('../models/index');
+const { db } = require('../db/connection');
+const musicianRouter = require('./routes/musicians');
+const bandsRouter = require('./routes/bands')
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//TODO: Create a GET /musicians route to return all musicians
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/musicians', musicianRouter);
+app.use('/bands', bandsRouter);
 
-
-
-
-
-
+// 
+// app.listen(port, () => {
+//   db.sync();
+//   console.log(`Listening at http://localhost:${port}/musicians`);
+// });
 module.exports = app;
